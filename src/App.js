c26135c00f92980c49d99a2ab19b029eea81050f
KimChaehyun <chyun1750@gmail.com>
2021-11-01 22:40:39 +0900

    1101

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-//import TabNavigation from './navigations/Tab';
-import ScreenLogin from './screens/login'
+// import TabNavigation from './navigations/Tab';
+// import ScreenLogin from './screens/login'
+import StackNavigation from './navigations/Stack';
 const App = () => {
   return (
     <NavigationContainer>
-      {/*<StackNavigation />*/}
-      {/*<TabNavigation />*/}
-      <ScreenLogin />
+      <StackNavigation />
+      {/* <TabNavigation /> */}
+      {/* <ScreenLogin /> */}
     </NavigationContainer>
   );
 };
