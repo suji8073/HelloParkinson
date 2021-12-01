837a6884b2d57d1354318cfb88a674c1ad037c31
KimChaehyun <chyun1750@gmail.com>
2021-12-02 01:51:03 +0900

    ch1202

--- src/navigations/Stack.js
+++ src/navigations/Stack.js
@@ -34,8 +34,6 @@ import move_3 from "../screens1/move_3";
 import move_4 from "../screens1/move_4";
 import move_play from "../screens1/move_play";
 
-
-import test from "../screens/test";
 const Stack = createStackNavigator();
 
 const StackNavigation = () => {
@@ -77,7 +75,6 @@ const StackNavigation = () => {
       <Stack.Screen name="move_3" component={move_3} />
       <Stack.Screen name="move_4" component={move_4} />
       <Stack.Screen name="move_play" component={move_play} />
-      <Stack.Screen name="test" component={test} />
       <Stack.Screen
         name="patient_profile_edit"
         component={patient_profile_edit}
