a86490f2d1306d5f5696bea2d1e6a5a35499a7f9
suji8073 <csj8073@gmail.com>
2022-01-29 20:47:01 +0900

    update!

--- src/screens1/yusanso_2.js
+++ src/screens1/yusanso_2.js
@@ -104,7 +104,6 @@ export default class yusanso_2 extends Component {
     this.setState({ startDisable: false });
     this.setState({ play: false });
     storeData(this.state.minutes_Counter, this.state.seconds_Counter);
-    console.log(parseInt(this.state.minutes_Counter));
     this.save_progress();
     this.props.navigation.navigate("move_5");
   };
@@ -119,7 +118,6 @@ export default class yusanso_2 extends Component {
         donecnt: parseInt(this.state.minutes_Counter),
       }),
     }).then((res) => {
-      console.log(res.status);
       if (res.status === 200) {
         console.log("저장 성공");
       }
