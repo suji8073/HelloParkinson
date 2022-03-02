d3cae81f25769931d08eef8a68058c1bb7af01b9
suji8073 <csj8073@gmail.com>
2022-03-02 18:42:44 +0900

    유산소운동 날짜와 함께 저장 완료

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import App from "./src/App";
-import React, { Component } from "react";
+import React from "react";
+import { AppState } from "react-native";
 import Loading from "./src/Loading";
+import PushController from "./src/PushController";
 
 export default class extends React.Component {
   state = {
@@ -18,13 +20,33 @@ export default class extends React.Component {
         if (response.status === 200) this.setState({ isLoading: false });
       });
     }, 3000);
+
+    //AppState는 앱이 ‘foreground’에 있는지, ‘background’에 있는지를 알려준다.
+    AppState.addEventListener("change", this.handleAppStateChange);
+  };
+
+  componentWillUnmount = () => {
+    AppState.removeEventListener("change", this.handleAppStateChange);
+  };
+
+  handleAppStateChange = (appState) => {
+    if (appState === "background") {
+      PushNotification.localNotificationSchedule({
+        message: "My Notification Message", // (required)
+        date: new Date(Date.now() + 60 * 1000), // in 60 secs
+      });
+    }
   };
 
   render() {
     if (this.state.isLoading) {
       return <Loading />;
     } else {
-      return <App />;
+      return (
+        <App>
+          <PushController />
+        </App>
+      );
     }
   }
 }
